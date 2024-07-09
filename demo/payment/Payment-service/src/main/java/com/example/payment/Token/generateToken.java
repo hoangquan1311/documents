cb9b0817398eba515e4b2e92382c9ca1c1bed69d
hoangquan <hoangquan131102@gmail.com>
2024-07-09 17:09:29 +0700

    add..

--- demo/payment/Payment-service/src/main/java/com/example/payment/Token/generateToken.java
+++
@@ -1,9 +0,0 @@
-package com.example.payment.Token;
-
-import java.util.UUID;
-
-public class generateToken {
-    public static String generateToken() {
-        return UUID.randomUUID().toString();
-    }
-}
