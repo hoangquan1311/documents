ca479e3dc657f63a745cae9630d22588fc9a974f
hoangquan <hoangquan131102@gmail.com>
2024-07-23 15:24:27 +0700

    add..

--- demo/fee-javacore/src/main/java/org/example/Main.java
+++ demo/fee-javacore/src/main/java/org/example/Main.java
@@ -14,19 +14,37 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class Main {
+    public Logger logger = LoggerFactory.getLogger(Main.class);
+    //    public static void main(String[] args) throws SchedulerException {
+//        FeeCommandTransactionService service = new FeeCommandTransactionService();
+//        service.addFeeCommandAndTransactions("hjawfhwqsnmkfu123894y222233", "20230623155051", "FC230623523226", 1000, 1000, "admin", "20230623155051");
+//        service.getFeeCommand("FC230623523226");
+//        service.updateFeeTransactions();
+//        service.updateChargeScanAndUpdateStatus();
+//
+//        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+//        JobDetail job = JobBuilder.newJob(Cronjob.class)
+//                .withIdentity("job1", "group1")
+//                .build();
+//        Trigger trigger = TriggerBuilder.newTrigger()
+//                .withIdentity("feeTransactionTrigger", "group1")
+//                .startNow()
+//                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+//                        .withIntervalInMinutes(3)
+//                        .repeatForever())
+//                .build();
+//        scheduler.scheduleJob(job, trigger);
+//
+//        scheduler.start();
+//    }
     public static void main(String[] args) throws IOException, SchedulerException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/fee", new FeeHttpHandler());
         server.createContext("/update-fee-transactions", new FeeHttpHandler());
         server.createContext("/update-charge-scan-and-status", new FeeHttpHandler());
         server.setExecutor(null);
-        server.start();
         System.out.println("Server started on port 8080");
-
         Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-        scheduler.start();
-        System.out.println("Scheduler started");
-
         JobDetail job = JobBuilder.newJob(Cronjob.class)
                 .withIdentity("job1", "group1")
                 .build();
@@ -34,9 +52,11 @@ public class Main {
                 .withIdentity("feeTransactionTrigger", "group1")
                 .startNow()
                 .withSchedule(SimpleScheduleBuilder.simpleSchedule()
-                        .withIntervalInMinutes(3)
+                        .withIntervalInMinutes(5)
                         .repeatForever())
                 .build();
         scheduler.scheduleJob(job, trigger);
+        scheduler.start();
+        server.start();
     }
 }
